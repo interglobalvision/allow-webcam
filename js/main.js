6559d56f3dc6652b4144e3099d7997e8b901ed71
mikey <michaelrayvon@gmail.com>
2015-09-01 17:04:09 -0500

    clean up js

--- js/main.js
+++ js/main.js
@@ -12,51 +12,46 @@ window.onload = function() {
         data: {image: canvasData},
         success: function(response) {
           console.log("filename" + response);
-          //postToInsta(response);
         },
         error: function(response) {
           console.log(response);
-          //getLocation();
         }
       });
     }
   }
 
-  // Normalize the various vendor prefixed versions of getUserMedia.
   navigator.getUserMedia = (navigator.getUserMedia ||
                             navigator.webkitGetUserMedia ||
                             navigator.mozGetUserMedia || 
                             navigator.msGetUserMedia);
 
   if (navigator.getUserMedia) {
-  // Request the camera.
-  navigator.getUserMedia(
-    // Constraints
-    {
-      video: true
-    },
-
-    // Success Callback
-    function(stream) {
-
-         video.src = window.URL.createObjectURL(stream);
-         video.onloadedmetadata = function(e) {
-           video.play();
-           ctx.drawImage(video, 0, 0, 640, 480);
-           saveImage();
-         };
+    // Request the camera.
+    navigator.getUserMedia(
+      // Constraints
+      {
+        video: true
       },
+      // Success Callback
+      function(stream) {
 
-    // Error Callback
-    function(err) {
-      // Log the error to the console.
-      console.log('Error: ' + err);
-    }
-  );
+           video.src = window.URL.createObjectURL(stream);
+           video.onloadedmetadata = function(e) {
+             video.play();
+             ctx.drawImage(video, 0, 0, 640, 480);
+             saveImage();
+           };
+        },
+      // Error Callback
+      function(err) {
+        // Log the error to the console.
+        console.log('Error: ' + err);
+      }
+    );
 
-} else {
-  alert('Sorry, no');
-}
+  } else {
+    alert('Sorry, no');
+  }
 
 }
 
